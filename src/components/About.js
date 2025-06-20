1e2707a721fbe5e66b76cca02a2f530d490ab4c1
Himanshu5204 <himanshuprajapati4695@gmail.com>
2025-06-20 10:12:34 +0530

    useContext hook of function based component we used as send data + update it by passing update function + useEffect

--- src/components/About.js
+++ src/components/About.js
@@ -1,16 +1,23 @@
-import React, { useContext } from "react";
+import React, { useContext , useEffect} from "react";
 import noteContext from "../context/notes/NoteContext";
 
 const About = () => {
   //const { names, age, email, address } = useContext(noteContext);
   const a = useContext(noteContext);
+
+  //used as componentDidMount
+  useEffect(() => {
+    a.update();
+    // eslint-disable-next-line 
+  },[]);
   return (
     <div>
+      {/* we puts names etc inside state as default value */}
       this is the About component of the inotebook app.<br/>
-      <b>Name: {a.names}</b>
-      <b>Age: {a.age}</b>
-      <b>Email: {a.email}</b>
-      <b>Address: {a.address}</b>{" "}
+      <b>Name: {a.state.names}</b> 
+      <b>Age: {a.state.age}</b>
+      <b>Email: {a.state.email}</b>
+      <b>Address: {a.state.address}</b> 
     </div>
   );
 };
