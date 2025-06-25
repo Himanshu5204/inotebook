57877be666373032c28dc5047976d866c6cf0761
Himanshu5204 <himanshuprajapati4695@gmail.com>
2025-06-25 14:17:48 +0530

    download as pdfs/txt funcctionality added

--- src/components/NoteItem.js
+++ src/components/NoteItem.js
@@ -1,6 +1,8 @@
 import React, { useContext } from 'react'; //rafce
 import noteContext from '../context/notes/NoteContext';
-import {FaThumbtack } from 'react-icons/fa';
+import { FaThumbtack } from 'react-icons/fa';
+import { saveAs } from 'file-saver';
+import jsPDF from 'jspdf';
 
 const NoteItem = (props) => {
   const context = useContext(noteContext); //for using context notestate.js ..one step up
@@ -30,6 +32,30 @@ const NoteItem = (props) => {
               title={note.pinned ? 'Unpin' : 'Pin'}
               style={{ color: note.pinned ? '#b00505' : '#888', transform: note.pinned ? 'rotate(-45deg)' : 'none' }}>
               <FaThumbtack />
+            </button>
+            {/* Export as Text */}
+            <button
+              className='btn btn-sm btn-outline-secondary mx-1'
+              onClick={() => {
+                const blob = new Blob([`Title: ${note.title}\n\n${note.description}`], {
+                  type: 'text/plain;charset=utf-8'
+                });
+                saveAs(blob, `${note.title}.txt`);
+              }}
+              title='Export as Text'>
+              TXT
+            </button>
+
+            {/* Export as PDF */}
+            <button
+              className='btn btn-sm btn-outline-danger mx-1'
+              onClick={() => {
+                const doc = new jsPDF();
+                doc.text(`Title: ${note.title}\n\n${note.description}`, 10, 10);
+                doc.save(`${note.title}.pdf`);
+              }}
+              title='Export as PDF'>
+              PDF
             </button>
           </div>
           <p className='card-text'>{note.description}</p>
